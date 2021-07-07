520e5169316ebc3d1775acec26b43b387b280d4e
Lucas Lima <lucaslima.contato00@gmail.com>
2021-07-07 07:26:33 -0300

    Getting data via json

--- src/components/MultiLineChart.jsx
+++ src/components/MultiLineChart.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react';
 import {Chart} from '../charts/Chart'
+import Temperature from '../jsons/Temperature'
+import Precipitation from '../jsons/Precipitation'
 
 const MultiLineChart = () => {
     const [temperature, setTemperature] = useState([])
     const [precipitation, setPrecipitation] = useState([])
 
     async function getAndSetTemperature() {
-        fetch("https://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/bra")
-            .then(response => response.json())
-            .then(data => setTemperature(data))
+        // fetch("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/bra")
+        //     .then(response => response.json())
+        //     .then(data => setTemperature(data))
+        setTemperature(Temperature)
     }
     function getAndSetPrecipitation() {
-        fetch("https://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/pr/year/bra")
-            .then(response => response.json())
-            .then(data => setPrecipitation(data))
+        // fetch("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/pr/year/bra")
+        //     .then(response => response.json())
+        //     .then(data => setPrecipitation(data))
+        setPrecipitation(Precipitation)
     }
 
     useEffect(() => {
